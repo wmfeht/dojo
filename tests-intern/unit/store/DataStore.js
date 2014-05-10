b854a9fea7388e7df0fbe29680fb3cf5b4c09289
wmfeht <wmfeht@outlook.com>
2014-05-10 16:59:54 -0500

    Converted unit tests for store/DataStore

--- tests-intern/unit/store/DataStore.js
+++ tests-intern/unit/store/DataStore.js
@@ -13,32 +13,34 @@ define([
 		readOnlyStore;
 
 	registerSuite({
-		name: "dojo/store/DataStore",
+		name: 'dojo/store/DataStore',
 
-		setup: function() {
-			two = { id: 2, name: "two", even: true, prime: true };
-			four = { id: 4, name: "four", even: true, prime: false };
+		setup: function () {
+			two = { id: 2, name: 'two', even: true, prime: true };
+			four = { id: 4, name: 'four', even: true, prime: false };
 		},
 
-		beforeEach: function() {
+		beforeEach: function () {
 			items = [
-				{ id: 1, name: "one", prime: false },
-				{ id: 2, name: "two", even: true, prime: true },
-				{ id: 3, name: "three", prime: true },
-				{ id: 4, name: "four", even: true, prime: false },
-				{ id: 5, name: "five", prime: true,
+				{ id: 1, name: 'one', prime: false },
+				{ id: 2, name: 'two', even: true, prime: true },
+				{ id: 3, name: 'three', prime: true },
+				{ id: 4, name: 'four', even: true, prime: false },
+				{ id: 5, name: 'five', prime: true,
 					children: [
-						{ _reference:1 },
-						{ _reference:2 },
-						{ _reference:3 }
-					]
-				}];
+						{ _reference: 1 },
+						{ _reference: 2 },
+						{ _reference: 3 }
+					]}
+                ];
 
 			dataStore = new ItemFileWriteStore({
 				data: {
 					items: items,
-					identifier: "id"
-				}});
+					identifier: 'id'
+				}
+            });
+
 			dataStore.fetchItemByIdentity({ identity: null });
 			store = new DataStore({ store: dataStore });
 
@@ -48,52 +50,52 @@ define([
 		},
 
 		'.get': [
-			function() {
-				assert.strictEqual(store.get(1).name, "one");
-				assert.strictEqual(store.get(4).name, "four");
+			function () {
+				assert.strictEqual(store.get(1).name, 'one');
+				assert.strictEqual(store.get(4).name, 'four');
 				assert.ok(store.get(5).prime);
-				assert.strictEqual(store.get(5).children[1].name, "two");
+				assert.strictEqual(store.get(5).children[1].name, 'two');
 			}
 		],
 		'.remove (async)': [
-			function() {
+			function () {
 				store.remove(4);
-				return store.query({ even: true }).then(function(results) {
+				return store.query({ even: true }).then(function (results) {
 					assert.strictEqual(results.length, 1);
 				});
 			}
 		],
 		'.query (async)': [
-			function() {
+			function () {
 				return store.query({ prime: true }).then(function (results) {
 					assert.strictEqual(results.length, 3);
-					assert.strictEqual(results[2].children[2].name, "three");
+					assert.strictEqual(results[2].children[2].name, 'three');
 				});
 			},
-			function() {
+			function () {
 				return store.query({ even: true }).then(function (results) {
 					assert.deepEqual(results[0], two);
 					assert.deepEqual(results[1], four);
-					assert.strictEqual("four", results[1].name);
+					assert.strictEqual('four', results[1].name);
 				});
 			}
 		],
 		'.put': {
-			'new': function() {
+			'new': function () {
 				store.put({
 					id: 6,
 					perfect: true
 				});
 				assert.isTrue(store.get(6).perfect);
 			},
-			'update': function() {
+			'update': function () {
 				var four = store.get(4);
 				four.square = true;
 				store.put(four);
 				four = store.get(4);
 				assert.isTrue(four.square);
 			},
-			'read only': function() {
+			'read only': function () {
 				assert.notOk(readOnlyStore.put);
 			}
 		}
